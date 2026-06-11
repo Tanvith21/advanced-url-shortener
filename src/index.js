4fc203bca358008fc17bbc912547f60478d15d56
Tanvith21 <tanvithgulla21@gmail.com>
2026-06-11 16:16:28 -0400

    Use Railway PORT env variable

--- src/index.js
+++ src/index.js
@@ -18,12 +18,14 @@ app.get("/", (req, res) => {
   res.json({ message: "URL Shortener API is running" });
 });
 
+const PORT = process.env.PORT || 3000;
+
 mongoose
   .connect(process.env.MONGO_URI)
   .then(() => {
     console.log("MongoDB connected");
-    app.listen(process.env.PORT, () => {
-      console.log(`Server running on port ${process.env.PORT}`);
+    app.listen(PORT, () => {
+      console.log(`Server running on port ${PORT}`);
     });
   })
   .catch((err) => console.error(err));
